abaf77a6ee9bc76fbeebdc0d185b0e3b2984d52f
Michelle Chen <mchen0062@gmail.com>
2020-12-04 12:07:45 -0800

    Completed header and conceptual questions

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,25 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+import axios from "axios"
+ 
+axios
+.get('https://lambda-times-api.herokuapp.com/topics')
+.then (res => {
+console.log(res.data)
+console.log(res.data.topics);
+})
+.catch(err => {
+console.log('you are staring at the error!');
+})
+ 
+const title = document.querySelector(".title");
+ 
+function tabMaker(topic) {
+const tab = document.createElement("div");
+tab.classList.add("tab");
+tab.textContent = `${topic}`;
+ 
+return tab;
+}
